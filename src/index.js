052c58933a50e1cb18db598f8f85aa1a667cc84e
Vramanouski <119852812+Vramanouski@users.noreply.github.com>
2023-01-18 20:55:53 +0300

    add code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
   
-openAndClosedPairs=bracketsConfig.map(item => item.join(''));
-
-  lengthToBeChecked = str.length / 2;
-  
-    for (let i=str.length; i>lengthToBeChecked*0; i--) {
-              openAndClosedPairs.forEach(newItem => {
-              str=str.replace(`${newItem}`, '');
+bracketsConfig=bracketsConfig.map(item=>item.join(''));
+    for (let i=str.length; i>0; i--) {
+      bracketsConfig.forEach(newItem => {
+        str=str.replace(`${newItem}`, '');
       });
     }
-   
-  return str.length == 0;
+    return str.length==0
   
   }
    
